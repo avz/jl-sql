1d002450a3c2c9f9511a1754860fa022fa56ea76
Artem Zaytsev <avz@nologin.ru>
2018-08-11 14:12:14 +0300

    Drop support of node v4 and v5

--- test/functional/all.sh
+++ test/functional/all.sh
@@ -10,10 +10,9 @@ repeat() {
 	awk 'BEGIN { for (i = 0; i < ARGV[1]; i++) print ARGV[2]; }' "$n" "$string"
 }
 
-for cmd in ./bin/jl-sql "node ./transpiled/main-node4-transpilled.js"; do
-	[ "$(repeat 100000 '{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c GROUP BY hello')" = '{"c":100000}' ] || exit 1
-	[ "$(repeat 10 '{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c GROUP BY hello')" = '{"c":10}' ] || exit 2
-	[ "$(repeat 10 '{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c FROM `/dev/stdin` GROUP BY hello')" = '{"c":10}' ] || exit 3
-	[ "$(repeat 10 '{"hello":"world"}{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c GROUP BY hello')" = '{"c":20}' ] || exit 4
-	[ "$(repeat 10 '{"hello":"world"}{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c FROM `/dev/stdin` GROUP BY hello')" = '{"c":20}' ] || exit 5
-done
+cmd=./bin/jl-sql
+[ "$(repeat 100000 '{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c GROUP BY hello')" = '{"c":100000}' ] || exit 1
+[ "$(repeat 10 '{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c GROUP BY hello')" = '{"c":10}' ] || exit 2
+[ "$(repeat 10 '{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c FROM `/dev/stdin` GROUP BY hello')" = '{"c":10}' ] || exit 3
+[ "$(repeat 10 '{"hello":"world"}{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c GROUP BY hello')" = '{"c":20}' ] || exit 4
+[ "$(repeat 10 '{"hello":"world"}{"hello":"world"}' | $cmd 'SELECT COUNT(*) AS c FROM `/dev/stdin` GROUP BY hello')" = '{"c":20}' ] || exit 5
